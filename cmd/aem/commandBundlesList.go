98f287df52eabee44fd36dfadbf261b99ba3f668
Jason Lentink <jason@mediamonks.com>
2019-03-08 09:57:30 +0000

    Add SymbolicName

--- cmd/aem/commandBundlesList.go
+++ cmd/aem/commandBundlesList.go
@@ -30,9 +30,9 @@ func (c *bundleListCommand) Execute(args []string) {
 	fmt.Printf("%s\n", bundles.Status)
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Id", "Name", "Version", "Category", "Status"})
+	t.AppendHeader(table.Row{"Id", "Name", "Symbolic Name", "Version", "Category", "Status"})
 	for _, bundle := range bundles.Data {
-		t.AppendRow(table.Row{bundle.ID, bundle.Name, bundle.Version, bundle.Category, bundle.State})
+		t.AppendRow(table.Row{bundle.ID, bundle.Name, bundle.SymbolicName, bundle.Version, bundle.Category, bundle.State})
 	}
 	t.Render()
 }
